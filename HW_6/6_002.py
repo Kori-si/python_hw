263e1cab694b68fc9264cd5ce5c0b3e350223749
Korinna Sinelnikova <103421831+Kori-si@users.noreply.github.com>
2022-08-17 17:09:09 +0200

    Add files via upload

---
+++ HW_6/6_002.py
@@ -0,0 +1,43 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+# num1 = int(input("Введите отрицательное число: "))
+# num2 = int(input("Введите положительное число: "))
+#
+# list_fact = []
+#
+# for i in range(num1, num2+1):
+#     list_fact.append(i)
+# print(list_fact)
+#
+# element = input("Введите индексы требуемых элементов (через пробел): ")
+# elements = element.split(" ")
+#
+# original_list = list_fact
+#
+# def mult_element(original_list: list, elements: list):
+#     mult= 1
+#     for element in elements:
+#         mult *= original_list[int(element)]
+#     return mult
+#
+# print(f"Произведение элементов на позициях {elements} равно {mult_element(list_fact, elements)}")
+
+num1 = int(input("Введите отрицательное число: "))
+num2 = int(input("Введите положительное число: "))
+
+list_fact = [i for i in range(num1, num2+1)]
+print(list_fact)
+
+element = input("Введите индексы требуемых элементов (через пробел): ")
+elements = element.split(" ")
+
+original_list = list_fact
+
+def mult_element(original_list: list, elements: list):
+    mult = 1
+    for element in elements:
+        mult *= original_list[int(element)]
+    return mult
+
+print(f"Произведение элементов на позициях {elements} равно {mult_element(list_fact, elements)}")
---
+++ HW_6/6_003.py
@@ -0,0 +1,41 @@
+# Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.
+# Пример:
+# Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
+
+# def sequence_list(number):
+#     list1= []
+#
+#     for n in range(1, number+1):
+#         list1.append((1 + 1 / n) ** n)
+#     return list1
+#
+# def sum_floats_in_list(list):
+#     sum = 0
+#
+#     for i in list:
+#         sum += i
+#
+#     return sum
+#
+# num = int(input('Введите N: '))
+# list = sequence_list(num)
+# sum = sum_floats_in_list(list)
+# print(f'Последовательность для N: \n{list}')
+# print(f'Сумма элементов последовательности = {sum}')
+
+def sum_floats_in_list(list):
+    sum = 0
+
+    for i in list:
+        sum += i
+
+    return sum
+
+num = int(input('\nВведите N: '))
+
+seqlam = lambda i: ((1 + 1 / i) ** i)
+list = [seqlam(i) for i in range(1, num+1)]
+
+sum = sum_floats_in_list(list)
+print(f'\nПоследовательность для заданного N: \n{list}')
+print(f'\nСумма элементов последовательности равна {sum}\n')
